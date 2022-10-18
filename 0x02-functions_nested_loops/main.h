175ab98fb59c353034096ddaf6bd7fd3413ed417
kelvin-karimi <mwaijr10@gmail.com>
2022-10-18 13:53:55 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -11,6 +12,6 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-
+int add(int, int);
 
 #endif
